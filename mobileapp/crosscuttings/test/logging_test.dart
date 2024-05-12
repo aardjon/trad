23a72f2630d8a935e581cc8c172fd2a8819e073c
Aardjon <58855074+aardjon@users.noreply.github.com>
2024-05-12 09:07:39 +0200

    ✅ Add and extend logging unit test: - Add test cases for the new log destinations - Refactor to a real blackbox test to better support potential future changes of package internals - Add an error test case

--- mobileapp/crosscuttings/test/logging_test.dart
+++ mobileapp/crosscuttings/test/logging_test.dart
@@ -1,48 +1,22 @@
-// Unit tests for the crosscuttings.di library
+///
+/// Unit tests for the crosscuttings.logging library
+///
 
 import 'dart:io';
 
-import 'package:logging/logging.dart' as loglib;
+import 'package:crosscuttings/logging/config.dart';
 import 'package:test/test.dart';
 
 import 'package:crosscuttings/logging.dart';
-import 'package:crosscuttings/logging/handlers.dart';
-
-abstract interface class ExampleInterface1 {
-  bool isSomething();
-}
-
-abstract interface class ExampleInterface2 {
-  bool isSomethingElse();
-}
-
-class ExampleImpl extends ExampleInterface1 {
-  @override
-  bool isSomething() {
-    return true;
-  }
-}
 
 /// Unit tests for the crosscuttings.logging component.
 void main() {
   /// Test cases for correct level propagation and filtering
   group('crosscuttings.logging.loglevels', () {
-    Directory? tempDir;
-    File? logFile;
-
     setUp(() {
-      // Create a log file directory
-      final String logFileName = "testexample.log";
-      tempDir = Directory.systemTemp.createTempSync('trad_test_');
-      logFile = File('${tempDir!.path}${Platform.pathSeparator}$logFileName');
-
       // Configure logging to write to a file
       final logConfig = LogConfiguration();
-      logConfig.destination = FileLogDestination(logFile!.path);
-    });
-
-    tearDown(() {
-      tempDir?.deleteSync(recursive: true);
+      logConfig.destination = MemoryLogDestination();
     });
 
     /// Ensures that all messages matching the log level configuration are be written to the log
@@ -70,36 +44,51 @@ void main() {
       logger.trace("Message 11");
       logger.trace("Message 12");
 
-      // The log file must only be created if messages are written at all
-      if (expectedMessageCount > 0) {
-        // Ensure that the log file has been written at all
-        expect(logFile!.existsSync(), isTrue);
+      // Check whether the log output is as expected
+      List<String> fileContent =
+          (logConfig.destination as MemoryLogDestination).loggedMessages;
 
-        // Read the created log file
-        List<String> fileContent = logFile!.readAsLinesSync();
+      // Ensure the message count is as expected
+      expect(fileContent.length, equals(expectedMessageCount));
 
-        // Ensure the message count is as expected
-        expect(fileContent.length, equals(expectedMessageCount));
-
-        // Ensure that all messages of the activated levels have been written in the correct
-        // order
-        for (int i = 1; i < expectedMessageCount; i++) {
-          expect(fileContent[i - 1], contains("Message $i"));
-        }
-      } else {
-        // No messages should have been written, so there shoiuldn't be a log file at all
-        expect(logFile!.existsSync(), isFalse);
+      // Ensure that all messages of the activated levels have been written in the correct order
+      for (int i = 1; i < expectedMessageCount; i++) {
+        expect(fileContent[i - 1], contains("Message $i"));
       }
     }
 
-    test('AllLevel', () => runTestCase(LogLevel.all, 12));
-    test('TraceLevel', () => runTestCase(LogLevel.trace, 12));
-    test('DebugLevel', () => runTestCase(LogLevel.debug, 10));
-    test('InfoLevel', () => runTestCase(LogLevel.info, 8));
-    test('WarningLevel', () => runTestCase(LogLevel.warning, 6));
-    test('ErrorLevel', () => runTestCase(LogLevel.error, 4));
-    test('FatalLevel', () => runTestCase(LogLevel.fatal, 2));
-    test('OffLevel', () => runTestCase(LogLevel.off, 0));
+    test(
+      'AllLevel',
+      () => runTestCase(LogLevel.all, 12),
+    );
+    test(
+      'TraceLevel',
+      () => runTestCase(LogLevel.trace, 12),
+    );
+    test(
+      'DebugLevel',
+      () => runTestCase(LogLevel.debug, 10),
+    );
+    test(
+      'InfoLevel',
+      () => runTestCase(LogLevel.info, 8),
+    );
+    test(
+      'WarningLevel',
+      () => runTestCase(LogLevel.warning, 6),
+    );
+    test(
+      'ErrorLevel',
+      () => runTestCase(LogLevel.error, 4),
+    );
+    test(
+      'FatalLevel',
+      () => runTestCase(LogLevel.fatal, 2),
+    );
+    test(
+      'OffLevel',
+      () => runTestCase(LogLevel.off, 0),
+    );
   });
 
   /// Special test cases for the LogConfiguration class
@@ -125,52 +114,97 @@ void main() {
     });
   });
 
-  /// Special test cases for testing the concrete handler implementations only
+  /// Special test cases to ensure that logging works correctly with all handler implementations
   group('crosscuttings.logging.handlers', () {
-    Directory? tempDir;
-    File? logFile;
+    const String exampleMessage = "Example Message";
+    const String exampleChannel = "test.channel";
 
-    setUp(() {
-      // Create a log file directory
-      final String logFileName = "testexample.log";
-      tempDir = Directory.systemTemp.createTempSync('trad_test_');
-      logFile = File('${tempDir!.path}${Platform.pathSeparator}$logFileName');
-    });
+    Logger configureLogging(LogDestination destination) {
+      final logConfig = LogConfiguration();
+      logConfig.globalLevel = LogLevel.debug;
+      logConfig.destination = destination;
 
-    tearDown(() {
-      tempDir?.deleteSync(recursive: true);
-    });
+      return Logger(exampleChannel);
+    }
 
-    loglib.LogRecord exampleRecord = loglib.LogRecord(
-      loglib.Level.SEVERE,
-      "Example Message",
-      "test.channel",
-    );
-
-    /// Test for the BlackholeLogHandler class.
+    /// Test for logging with the Blackhole destination.
     /// This handler shall do nothing, so this test mainly ensures that it does not throw.
     void testBlackholeHandler() {
-      LogHandler handler = BlackholeLogHandler();
-      handler.handleRecord(exampleRecord);
+      Logger logger = configureLogging(BlackholeLogDestination());
+      logger.warning(exampleMessage);
     }
 
-    /// Test for the FileLogHandler class.
+    /// Test for logging with the the File destination.
     /// Ensures that a log file is written and contains the sent log message
     void testFileHandler() {
-      LogHandler handler = FileLogHandler(logFile!.path);
-      handler.handleRecord(exampleRecord);
+      // Create a log file directory
+      final String logFileName = "testexample.log";
+      Directory tempDir = Directory.systemTemp.createTempSync('trad_test_');
+      File logFile =
+          File('${tempDir.path}${Platform.pathSeparator}$logFileName');
+
+      // Log a single message
+      Logger logger = configureLogging(FileLogDestination(logFile.path));
+      logger.warning(exampleMessage);
 
       // The log file must have been written
-      expect(logFile!.existsSync(), isTrue);
+      expect(logFile.existsSync(), isTrue);
 
       // Ensure the file content is as expected
-      List<String> fileContent = logFile!.readAsLinesSync();
+      List<String> fileContent = logFile.readAsLinesSync();
       expect(fileContent.length, equals(1));
-      expect(fileContent[0], contains(exampleRecord.loggerName));
-      expect(fileContent[0], contains(exampleRecord.message));
+      expect(fileContent[0], contains(exampleMessage));
+      expect(fileContent[0], contains(exampleChannel));
+
+      // Delete the temporary directory
+      tempDir.deleteSync(recursive: true);
     }
 
+    /// Test for logging with the Memory destination.
+    /// Ensures that the sent log message is stored within the destination object
+    void testMemoryHandler() {
+      MemoryLogDestination destination = MemoryLogDestination();
+      Logger logger = configureLogging(destination);
+      logger.warning(exampleMessage);
+
+      expect(destination.loggedMessages.length, equals(1));
+      expect(destination.loggedMessages[0], contains(exampleMessage));
+      expect(destination.loggedMessages[0], contains(exampleChannel));
+    }
+
+    /// Test for logging with the Console destination.
+    /// Ensures that the sent message is really print()ed,
+    void testConsoleHandler() {
+      expect(
+        () {
+          Logger logger = configureLogging(ConsoleLogDestination());
+          logger.warning(exampleMessage);
+        },
+        prints(contains(exampleMessage)),
+      );
+    }
+
+    // Run all the defined test cases
     test('BlackholeLogHandler', testBlackholeHandler);
     test('FileLogHandler', testFileHandler);
+    test('MemoryLogHandler', testMemoryHandler);
+    test('ConsoleLogHandler', testConsoleHandler);
+  });
+
+  /// Special test cases to ensure the correct behaviour in error cases
+  group('crosscuttings.logging.errors', () {
+    /// Ensures the correct behaviour (ArgumentError) in case of an unknown LogDestination being
+    /// used. If this tets fails, you probably added a new [LogDestination] class without
+    /// providing a corresponding [LogHandler] implementation.
+    test('UnknownLogDestination', () {
+      final logConfig = LogConfiguration();
+      expect(() {
+        logConfig.destination = _UnknownLogDestination();
+      }, throwsArgumentError);
+    });
   });
 }
+
+/// A new LogDestination without a correspoding handler implementation.
+/// Used for testing some error behaviour.
+class _UnknownLogDestination extends LogDestination {}
