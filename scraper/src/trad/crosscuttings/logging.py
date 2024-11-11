fdd0910b78518ad64dc3fd6eb048a73204eaf1b2
Aardjon <58855074+aardjon@users.noreply.github.com>
2024-11-11 20:57:59 +0100

    feature: Add scraper.crosscuttings.logging module for configuring the logging output

---
+++ scraper/src/trad/crosscuttings/logging.py
@@ -0,0 +1,19 @@
+"""
+Module for configuraing the logging system.
+
+trad uses the normal Python logging framework, that's why this module is only about configuring this
+tool to our needs.
+"""
+
+from logging import basicConfig
+
+
+def configure_logging(default_log_level: int | str):
+    """
+    Configure the logging framework (i.e. the root logger) to output all messages with at the given
+    log level.
+    """
+    basicConfig(
+        format="[%(asctime)s][%(levelname)s][%(name)s] %(message)s",
+        level=default_log_level,
+    )
