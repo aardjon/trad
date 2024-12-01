a001ca96aac5d2024c38b8898b9b2a072dc09ac7
Aardjon <58855074+aardjon@users.noreply.github.com>
2024-12-01 20:46:38 +0100

    test: Add integration test for creating the DB file via the pipe

---
+++ scraper/test/integration/test_pipe_dbcreation.py
@@ -0,0 +1,66 @@
+"""
+Integration test for the database creation via the V1 pipe and the underlying database
+infrastructure.
+"""
+
+import datetime
+from pathlib import Path
+from sqlite3 import connect
+from typing import Final
+
+from trad.adapters.pipes.db_v1.pipe import DbSchemaV1Pipe
+from trad.core.entities import Post, Route, Summit
+from trad.infrastructure.sqlite3db import Sqlite3Database
+
+
+def test_schema_v1_db_creation(tmp_path: Path) -> None:
+    post_date: Final = datetime.datetime.now(tz=datetime.UTC)
+    post_rating: Final = 2
+    expected_index_count: Final = 2  # 'peaks' and 'routes' tables each have one user-defined index
+
+    pipe = DbSchemaV1Pipe(output_directory=tmp_path, database_boundary=Sqlite3Database())
+    pipe.initialize_pipe()
+    pipe.add_summit_data(Summit(name="Falkenturm"))
+    pipe.add_route_data(summit_name="Falkenturm", route=Route(route_name="AW", grade="II"))
+    pipe.add_post_data(
+        summit_name="Falkenturm",
+        route_name="AW",
+        post=Post(
+            user_name="John Doe",
+            comment="This is great!",
+            post_date=post_date,
+            rating=post_rating,
+        ),
+    )
+
+    expected_db_file = tmp_path.joinpath("routedb_v1.sqlite")
+    assert expected_db_file.exists()
+
+    connection = connect(str(expected_db_file))
+
+    # Ensure the summit has been added
+    result_set = list(connection.execute("SELECT peak_name FROM peaks"))
+    assert len(result_set) == 1
+    assert result_set[0][0] == "Falkenturm"
+
+    # Ensure the route has been added
+    result_set = list(connection.execute("SELECT route_name, route_grade FROM routes"))
+    assert len(result_set) == 1
+    assert result_set[0][0] == "AW"
+    assert result_set[0][1] == "II"
+
+    # Ensure the post has been added
+    result_set = list(connection.execute("SELECT user_name, comment, post_date, rating FROM posts"))
+    assert len(result_set) == 1
+    assert result_set[0][0] == "John Doe"
+    assert result_set[0][1] == "This is great!"
+    assert datetime.datetime.fromisoformat(result_set[0][2]) == post_date
+    assert result_set[0][3] == post_rating
+
+    # Ensure that all indices have been created (ignoring the "autoindex" ones)
+    result_set = list(
+        connection.execute(
+            "SELECT * FROM sqlite_master WHERE type='index' AND name NOT LIKE '%autoindex%'"
+        )
+    )
+    assert len(result_set) == expected_index_count
