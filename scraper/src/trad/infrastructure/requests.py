25a07f4374ca947604394b31919ba7786faaf49a
Aardjon <58855074+aardjon@users.noreply.github.com>
2024-12-01 18:46:28 +0100

    feat: Implement the http component based on the 'requests' library

---
+++ scraper/src/trad/infrastructure/requests.py
@@ -0,0 +1,26 @@
+"""
+Implementation of a generic HTTP networking component.
+"""
+
+from typing import override
+
+import requests
+
+from trad.adapters.boundaries.http import HttpNetworkingBoundary
+
+
+class RequestsHttp(HttpNetworkingBoundary):
+    """
+    Implementation of HTTP requests using the `requests` library.
+
+    Library documentation: https://docs.python-requests.org/en/latest/index.html
+    """
+
+    @override
+    def retrieve_text_resource(self, url: str) -> str:
+        page = requests.get(
+            url=url,
+            headers={"User-Agent": "Thunder Client (https://www.thunderclient.com)"},
+            timeout=5,
+        )
+        return page.text
