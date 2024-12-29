1988e1019e01eae8ddb2be77c1dda737942eec87
Aardjon <58855074+aardjon@users.noreply.github.com>
2024-12-29 19:41:13 +0100

    fix: Only instantiate Filter instances when they are really needed
    This is a simple performance and memory optimization.
    
    This commit partially reverts efcb058 which fixed some linter issues. However, the MyPy issue that was avoided here is a known
    False Positive and can therefore simply be ignored.

--- scraper/src/trad/adapters/filters/factory.py
+++ scraper/src/trad/adapters/filters/factory.py
@@ -2,7 +2,7 @@
 Implementation of the FilterFactory component.
 """
 
-from typing import override
+from typing import Final, override
 
 from trad.adapters.filters.initialization import PipeInitializingFilter
 from trad.adapters.filters.optimization import PipeOptimizingFilter
@@ -14,18 +14,28 @@ from trad.crosscuttings.di import DependencyProvider
 class AllFiltersFactory(FilterFactory):
     """
     The factory for creating all filters that should be executed.
+
+    Filters are only instantiated when they are used.
     """
 
     def __init__(self, dependency_provider: DependencyProvider) -> None:
         """
         Create a new FilterFactory with the given [dependency_provider].
         """
-        self.__filters = [
-            PipeInitializingFilter(dependency_provider),
-            TeufelsturmDataFilter(dependency_provider),
-            PipeOptimizingFilter(dependency_provider),
-        ]
+        self.__dependency_provider = dependency_provider
 
     @override
     def create_filters(self, stage: FilterStage) -> list[Filter]:
-        return [f for f in self.__filters if f.get_stage() == stage]
+        filter_classes: Final = [
+            PipeInitializingFilter,
+            TeufelsturmDataFilter,
+            PipeOptimizingFilter,
+        ]
+        return [
+            # Ignoring the MyPy warning "Cannot instantiate abstract class" here because it is a
+            # known False Positive which will hopefully go away in some future version.
+            # See also: https://github.com/python/mypy/issues/15554 type: ignore[abstract]
+            filter_class(self.__dependency_provider)  # type: ignore[abstract]
+            for filter_class in filter_classes
+            if filter_class.get_stage() == stage
+        ]
