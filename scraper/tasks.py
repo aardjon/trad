5ebe549181d03cd7a40faebdc41cc3c2567c94e9
Aardjon <58855074+aardjon@users.noreply.github.com>
2025-06-29 07:57:19 +0200

    fix: Use a context manager for reading from files (linter warning)

--- scraper/tasks.py
+++ scraper/tasks.py
@@ -211,7 +211,7 @@ def upgrade(context: Context) -> None:
 
 
 @task
-def version(context: Context, version: str) -> None:
+def version(_context: Context, version: str) -> None:
     """
     Set the scraper application version to be `version`, which should be of the format
     "MAJOR.MINOR.PATCH" (e.g. "1.2.3") and must match the Python Software Version Specification
@@ -222,11 +222,12 @@ def version(context: Context, version: str) -> None:
         """
         Replace all lines starting with `line_head` with `replacement` within the given `file`.
         """
-        for line in fileinput.input(file, inplace=True):
-            if line.startswith(line_head):
-                print(replacement)
-            else:
-                print(line, end="")
+        with fileinput.input(file, inplace=True) as f:
+            for line in f:
+                if line.startswith(line_head):
+                    print(replacement)
+                else:
+                    print(line, end="")
 
     # Update appmeta.py (source code)
     replace_line_in_file(
