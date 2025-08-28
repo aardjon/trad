dc40bdfab4fe10e58e535a816171d0adbc77f216
Aardjon <58855074+aardjon@users.noreply.github.com>
2025-08-28 11:32:34 +0200

    refactor: Refactoring of the OsmSummitDataFilter class
    - Use the new OsmApiReceiver for requesting OSM
    - Prepare the internal structure for adding relation support

--- scraper/src/trad/filters/osm.py
+++ scraper/src/trad/filters/osm.py
@@ -20,10 +20,10 @@ from pydantic.fields import Field
 from pydantic.main import BaseModel
 from pydantic.type_adapter import TypeAdapter
 
-from trad.adapters.boundaries.http import HttpNetworkingBoundary, HttpRequestError, JsonData
+from trad.adapters.boundaries.http import HttpNetworkingBoundary, HttpRequestError
 from trad.core.boundaries.filters import Filter, FilterStage
 from trad.core.boundaries.pipes import Pipe
-from trad.core.entities import Summit
+from trad.core.entities import GeoPosition, Summit
 from trad.core.errors import DataProcessingError, DataRetrievalError, MergeConflictError
 from trad.crosscuttings.di import DependencyProvider
 
@@ -161,15 +161,20 @@ class OsmSummitDataFilter(Filter):
 
     Using the positions from OSM has the advantage that when sending it to some OSM based map later
     on the mobile device, the point is exactly on the summit and not "somewhere near".
+
+    In general, this implementation tries to do as less OSM queries as possible, and to reduce the
+    transfered data amount to a minimum.
     """
 
-    _NOMINATIM_API_ENDPOINT: Final = "https://nominatim.openstreetmap.org/search"
-    _OVERPASS_API_ENDPOINT: Final = "https://overpass-api.de/api/interpreter"
+    _OVERPASS_PEAK_NODE_TAGS: Final = {"natural": "peak"}
+    """ OSM node tags by which we recognize a single summit point. """
 
     @override
     def __init__(self, dependency_provider: DependencyProvider) -> None:
         super().__init__(dependency_provider)
-        self._http_boundary = dependency_provider.provide(HttpNetworkingBoundary)
+        self._osm_api_receiver = OsmApiReceiver(
+            http_boundary=dependency_provider.provide(HttpNetworkingBoundary)
+        )
 
     @staticmethod
     @override
@@ -183,68 +188,41 @@ class OsmSummitDataFilter(Filter):
     @override
     def execute_filter(self, pipe: Pipe) -> None:
         _logger.debug("'%s' filter started", self.get_name())
-        nominatim_json_data = self.__get_nominatim_area_data()
-        area_id = self.__process_nominatim_response(nominatim_json_data)
-        overpass_json_data = self.__get_overpass_summit_nodes(area_id)
-        summits = self.__process_overpass_response(overpass_json_data)
-        self.__store_summits(pipe, summits)
+        area_id = self.__get_area_id()
+
+        # Get all OSM nodes for that area
+        osm_elements = self.__get_osm_summit_elements(area_id)
+        _logger.debug("Retrieved %d OSM elements", len(osm_elements))
+
+        # Create Summit objects
+        summits_from_nodes = self.__create_summits_from_nodes(osm_elements)
+
+        # Send all summits to the pipe
+        self.__store_summits(pipe, summits_from_nodes)
+        _logger.debug("Processed summits from %d nodes", len(osm_elements))
         _logger.debug("'%s' filter finished", self.get_name())
 
-    def __get_nominatim_area_data(self) -> JsonData:
+    def __get_area_id(self) -> int:
         area_name: Final = "Sächsische Schweiz"
-        _logger.debug("Retrieving ID for area '%s' from Nominatim", area_name)
-        try:
-            response_json = self._http_boundary.retrieve_json_resource(
-                url=self._NOMINATIM_API_ENDPOINT,
-                url_params={"q": area_name, "limit": 1, "format": "jsonv2"},
-            )
-        except HttpRequestError as e:
-            raise DataRetrievalError("Nominatim request failed") from e
-        return response_json
+        return int(self._osm_api_receiver.retrieve_area_by_name(area_name).osm_id)
 
-    def __process_nominatim_response(self, json_data: JsonData) -> int:
-        type_adapter = TypeAdapter(list[_NominatimArea])
-        try:
-            area_elements = type_adapter.validate_json(json_data, strict=True)
-        except ValidationError as e:
-            raise DataProcessingError("Retrieved unexpected data from Nominatim") from e
-        if not (area_elements := type_adapter.validate_json(json_data, strict=True)):
-            raise DataProcessingError("Received an empty area list from Nominatim")
-        return int(area_elements[0].osm_id)
-
-    def __get_overpass_summit_nodes(self, area_id: int) -> JsonData:
-        node_tags: Final = {
-            "natural": "peak",
-            "climbing:trad": "yes",
+    def __get_osm_summit_elements(self, area_id: int) -> list[_OverpassElement]:
+        common_tags: Final = {"sport": "climbing", "climbing:trad": "yes"}
+        element_filter: Final = {
+            _OsmObjectTypes.node: self._OVERPASS_PEAK_NODE_TAGS | common_tags,
         }
-        _logger.debug("Querying all summits within area ID %s from Overpass", area_id)
-        tag_filter = "".join(f'["{t}"="{v}"]' for t, v in node_tags.items())
-        query = f"""
-            [out:json][timeout:25];
-            area({area_id})->.searchArea;
-            (
-                node{tag_filter}(area.searchArea);
-            );
-            out body;
+        return self._osm_api_receiver.retrieve_elements_from_area(area_id, element_filter)
+
+    def __create_summits_from_nodes(self, osm_nodes: Iterable[_OverpassNode]) -> Iterator[Summit]:
         """
-        try:
-            return self._http_boundary.retrieve_json_resource(
-                url=self._OVERPASS_API_ENDPOINT,
-                url_params={},
-                query_content=f"data={query}",
+        Creates (and yields) a Summit object for each node in `osm_nodes`.
+        """
+        for summit_element in osm_nodes:
+            yield Summit(
+                official_name=summit_element.tags.name,
+                alternate_names=summit_element.tags.get_alternate_names(),
+                position=GeoPosition.from_decimal_degree(summit_element.lat, summit_element.lon),
             )
-        except HttpRequestError as e:
-            raise DataRetrievalError("Overpass request failed") from e
-
-    def __process_overpass_response(self, json_data: JsonData) -> Iterator[Summit]:
-        try:
-            osm_response = _OverpassResponse.model_validate_json(json_data, strict=True)
-        except ValidationError as e:
-            raise DataProcessingError("Retrieved unexpected data from Overpass") from e
-
-        _logger.debug("Processing %d summits", len(osm_response.elements))
-        for element in osm_response.elements:
-            yield element.create_summit()
 
     def __store_summits(self, pipe: Pipe, summits: Iterable[Summit]) -> None:
         for summit in summits:
