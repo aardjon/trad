c87a0540e0ad96bf49dcc2f49da641c315bc277b
Aardjon <58855074+aardjon@users.noreply.github.com>
2025-09-13 12:49:22 +0200

    feat: Implement the new openExternalMapsApp system adapter method, adding a new infrastructure boundary

--- mobileapp/adapters/lib/sysenv.dart
+++ mobileapp/adapters/lib/sysenv.dart
@@ -6,8 +6,10 @@ library;
 import 'dart:io';
 
 import 'package:core/boundaries/sysenv.dart';
+import 'package:core/entities/geoposition.dart';
 import 'package:crosscuttings/di.dart';
 
+import 'boundaries/external_apps.dart';
 import 'boundaries/paths.dart';
 
 /// The system environment adapter component.
@@ -19,14 +21,24 @@ class SystemEnvironment implements SystemEnvironmentBoundary {
   /// information.
   final PathProviderBoundary _pathProviderBoundary;
 
+  /// Interface for interacting with other, external applications on the same device.
+  final ExternalAppsBoundary _externalAppsBoundary;
+
   /// Constructor for using the dependency provider given as [di] to obtain dependencies from other
   /// rings.
   SystemEnvironment(DependencyProvider di)
-    : _pathProviderBoundary = di.provide<PathProviderBoundary>();
+    : _pathProviderBoundary = di.provide<PathProviderBoundary>(),
+      _externalAppsBoundary = di.provide<ExternalAppsBoundary>();
 
   @override
   Future<String> getAppDataPath() async {
     Directory appDataDir = await _pathProviderBoundary.getAppDataDir();
     return appDataDir.path;
+  }
+
+  @override
+  Future<void> openExternalMapsApp(GeoPosition markPosition) async {
+    Uri geoUri = Uri(scheme: 'geo', path: '${markPosition.latitude},${markPosition.longitude}');
+    await _externalAppsBoundary.openGeoUri(geoUri);
   }
 }
