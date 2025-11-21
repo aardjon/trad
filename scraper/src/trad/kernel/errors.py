e5df5dc3cfa90c00666ea2322c60a6c1e416a77e
Aardjon <58855074+aardjon@users.noreply.github.com>
2025-11-21 08:44:04 +0100

    refactor: Derive MergeConflictError from FilterError, not from PipeDataError

--- scraper/src/trad/kernel/errors.py
+++ scraper/src/trad/kernel/errors.py
@@ -26,9 +26,34 @@ class DataProcessingError(FilterError):
     """
 
 
+class MergeConflictError(DataProcessingError):
+    """
+    Raised when a value cannot be processed because it already contains a different (conflicting)
+    value (and the conflict cannot be resolved automatically).
+    """
+
+    def __init__(self, object_type: str, object_name: str, conflicting_attribute: str):
+        """
+        Creates a new Exception notifying about a conflict on the `conflicting_attribute` property
+        of an `object_type` object with the name `object_name`. `object_type` is e.g. "route" or
+        "summit", `conflicting_attribute` is the name of the conflicting data field.
+        """
+        super().__init__()
+        self._object_type = object_type
+        self._object_name = object_name
+        self._conflicting_attribute = conflicting_attribute
+
+    @override
+    def __str__(self) -> str:
+        return (
+            f"Cannot merge {self._object_type} data for '{self._object_name}' because of "
+            f"conflicting {self._conflicting_attribute} values."
+        )
+
+
 class PipeDataError(Exception):
     """
-    Raised by Pipes when they cannot process (e.g. store) a certain value.
+    Raised by Pipes when they cannot fulfil a certain request.
     """
 
 
@@ -50,31 +75,6 @@ class EntityNotFoundError(PipeDataError):
     @override
     def __str__(self) -> str:
         return f"Referenced object '{self._object_name}' not found. Did you forget to add it?"
-
-
-class MergeConflictError(PipeDataError):
-    """
-    Raised when a value cannot be added to a Pipe because it already contains a different
-    (conflicting) value (and the conflict cannot be resolved automatically).
-    """
-
-    def __init__(self, object_type: str, object_name: str, conflicting_attribute: str):
-        """
-        Creates a new Exception notifying about a conflict on the `conflicting_attribute` property
-        of an `object_type` object with the name `object_name`. `object_type` is e.g. "route" or
-        "summit", `conflicting_attribute` is the name of the conflicting data field.
-        """
-        super().__init__()
-        self._object_type = object_type
-        self._object_name = object_name
-        self._conflicting_attribute = conflicting_attribute
-
-    @override
-    def __str__(self) -> str:
-        return (
-            f"Cannot merge {self._object_type} data for '{self._object_name}' because of "
-            f"conflicting {self._conflicting_attribute} values."
-        )
 
 
 class InvalidStateError(Exception):
