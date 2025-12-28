40b1fdd8400b77557f64717de9edd0e3d66a2420
Aardjon <58855074+aardjon@users.noreply.github.com>
2025-12-28 10:38:07 +0100

    feat: Extend the PathProviderBoundary interface with a new getTempDir() method
    This also improves documentation and adds unit tests.

--- mobileapp/adapters/lib/boundaries/paths.dart
+++ mobileapp/adapters/lib/boundaries/paths.dart
@@ -9,5 +9,18 @@ import 'dart:io';
 /// Abstract interface providing system specific file system paths for certain usages.
 abstract interface class PathProviderBoundary {
   /// Returns the full path to the application data directory.
+  ///
+  /// This is a directory for persisting application specific data, it must not be modified by other
+  /// applications. The operating system may (or may not) delete it when the application is
+  /// uninstalled or reset, though. Depending on the underlying platform, other users and/or
+  /// applications may be able to read its contents or not.
   Future<Directory> getAppDataDir();
+
+  /// Returns the full path to the temporary directory.
+  ///
+  /// The contents of this directory are not persisted and may be wiped by the operating system at
+  /// any time, e.g. on system reboot. The application is guaranteed to have read and write access
+  /// to it. The directory may be shared with other applications or not, so beware name collisions
+  /// when creating files there.
+  Future<Directory> getTempDir();
 }
