4d679983dc620caeef52f76ccc5a2b9b9cd18dad
Aardjon <58855074+aardjon@users.noreply.github.com>
2026-04-12 19:03:18 +0200

    fix(scraper): Merge conflict error message contains wrong attribute name

--- scraper/src/trad/application/filters/regular/merge.py
+++ scraper/src/trad/application/filters/regular/merge.py
@@ -293,7 +293,7 @@ class _SummitMerger(_EntityMerger[_SummitRelatedData]):
             if source.sector.rank < target.sector.rank:
                 target.sector = source.sector
             elif source.sector.rank == target.sector.rank:
-                raise MergeConflictError("summit", source.name, "ranked_sector")
+                raise MergeConflictError("summit", source.name, "sector")
 
 
 class _RouteMerger(_EntityMerger[_RouteRelatedData]):
