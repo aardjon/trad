a9695ba88a5f6a5e0f570f95f01be2fe0da3c494
Aardjon <58855074+aardjon@users.noreply.github.com>
2026-04-18 17:37:44 +0200

    feat(scraper): Keep HTTP server connections alive to improve performance
    This is done by reusing the Sessions objects per host. It speeds up network requests a lot
    by reducing the overhead for reconnecting over and over again, and reduces the scraper run time
    from about 55min to about 20min (mainly because the teufelsturm filter is now much faster).

--- scraper/src/trad/infrastructure/requests.py
+++ scraper/src/trad/infrastructure/requests.py
@@ -6,7 +6,9 @@ Implementation of a generic HTTP networking component.
 # checked by Mypy, so it's not turned off completely.
 # pylint: disable=no-member
 
+from collections.abc import Callable
 from typing import Final, override
+from urllib.parse import urlsplit
 
 import requests
 
@@ -24,6 +26,9 @@ class RequestsHttp(HttpNetworkingBoundary):
     `requests`, this component ensures that certain connection settings like timeouts or user agent
     strings are the same for all HTTP connections.
 
+    Furthermore, HTTP connections are reused (kept open) for each host, because we expect several
+    subsequent requests to the same host in most cases.
+
     Library documentation: https://docs.python-requests.org/en/latest/index.html
     """
 
@@ -33,7 +38,24 @@ class RequestsHttp(HttpNetworkingBoundary):
     """
 
     _REQUEST_TIMEOUT: Final = 60
-    """ The HTTP request timeout in seconds. """
+    """
+    The HTTP request timeout in seconds.
+    This value is used as both *connection*  and *read*  timeout for HTTP requests (see
+    https://docs.python-requests.org/en/latest/user/advanced/#timeouts more information).
+    """
+
+    def __init__(self, session_factory: Callable[[], requests.Session] | None = None) -> None:
+        """
+        Initializes a new RequestsHttp component instance.
+
+        The new instance uses `session_factory` to create new HTTP sessions, or the default
+        constructor as default. This parameter is mainly there for injection special (e.g. mocked)
+        Sessions from unit tests, and should not be set in normal production code.
+        """
+        self._session_factory = session_factory or requests.Session
+        """ Factory function for creating a new Session instance."""
+        self._sessions: dict[str, requests.Session] = {}
+        """ Session cache: Key is the host, value is the Sesison instance to use. """
 
     @override
     def retrieve_text_resource(
@@ -44,8 +66,8 @@ class RequestsHttp(HttpNetworkingBoundary):
         response = self._retrieve_resource(
             url=url,
             url_params=url_params,
-            additional_headers={},
             query_content=None,
+            configure_new_session=lambda _: None,
         )
         return response.text
 
@@ -56,11 +78,14 @@ class RequestsHttp(HttpNetworkingBoundary):
         url_params: dict[str, str | int] | None = None,
         query_content: str | None = None,
     ) -> JsonData:
+        def configure_session(session: requests.Session) -> None:
+            session.headers.update({"Accept": "application/json"})
+
         response = self._retrieve_resource(
             url=url,
             url_params=url_params,
-            additional_headers={"Accept": "application/json"},
             query_content=query_content,
+            configure_new_session=configure_session,
         )
         return JsonData(response.text)
 
@@ -68,20 +93,20 @@ class RequestsHttp(HttpNetworkingBoundary):
         self,
         url: str,
         url_params: dict[str, str | int] | None,
-        additional_headers: dict[str, str],
         query_content: str | None,
+        configure_new_session: _ConfigureSessionFunc,
     ) -> requests.models.Response:
         """
         Does the actual HTTP request, checks the response and returns the Response object (so that
         callers may do additional checks, if necessary). Raises HttpRequestError in case of any
         errors.
         """
-        headers = self._USER_AGENT_HEADER | additional_headers
+        session = self._get_session_for_url(url, configure_new_session)
+
         try:
-            response = requests.get(
+            response = session.get(
                 url=url,
                 params=url_params,
-                headers=headers,
                 data=query_content,
                 timeout=self._REQUEST_TIMEOUT,
             )
@@ -94,3 +119,32 @@ class RequestsHttp(HttpNetworkingBoundary):
                 f"Unexpected HTTP response {response.status_code}: {response.reason}"
             )
         return response
+
+    def _get_session_for_url(
+        self,
+        url: str,
+        configure_new_session: _ConfigureSessionFunc,
+    ) -> requests.Session:
+        """
+        Returns the requests session to use for requesting the given URL.
+
+        Sessions are cached in `self._sessions` and reused for all requests to the same host. A new
+        session is created when requesting a host for the first time.
+        """
+        host = urlsplit(url).hostname
+        if not host:
+            raise ValueError(f"Unable to parse hostname from URL {url}")
+
+        session = self._sessions.get(host)
+        if session is None:
+            session = self._session_factory()
+            session.headers.update(self._USER_AGENT_HEADER)
+            configure_new_session(session)
+            self._sessions[host] = session
+        return session
+
+
+_ConfigureSessionFunc = Callable[[requests.Session], None]
+"""
+Signature of a function that configures a (newly created) requests.Session object as necessary
+"""
--- scraper/test/trad/infrastructure/test_requests.py
+++ scraper/test/trad/infrastructure/test_requests.py
@@ -2,21 +2,22 @@
 Unit tests for the trad.infrastructure.requests module.
 """
 
-from collections.abc import Callable
+from collections.abc import Callable, Iterator
 from typing import Final
-from unittest.mock import ANY, Mock, patch
+from unittest.mock import Mock
+from urllib.parse import urlsplit
 
 import pytest
 from requests.exceptions import ConnectionError as RequestsConnectionError
 from requests.exceptions import HTTPError, Timeout
 from requests.models import Response
+from requests.sessions import Session
 
 from trad.application.boundaries.http import HttpRequestError
 from trad.infrastructure.requests import RequestsHttp
 from trad.kernel.appmeta import APPLICATION_NAME, APPLICATION_VERSION
 
 
-@patch("trad.infrastructure.requests.requests.get")
 class TestRequestsHttp:
     """
     Unit tests for the RequestsHttp component class.
@@ -54,9 +55,9 @@ class TestRequestsHttp:
     )
     def test_retrieve_text_resource_good(
         self,
-        mocked_requests_get: Mock,
         url_params: dict[str, str | int] | None,
         response_data: str,
+        requests_session: Mock,
     ) -> None:
         """
         Ensures the correct behaviour of the retrieve_text_resource() method for normal use cases:
@@ -65,8 +66,8 @@ class TestRequestsHttp:
          - The URL and their parameters must be forwarded correctly
          - The response content is returned as-is
         """
-        mocked_requests_get.return_value = Mock(Response, status_code=200, text=response_data)
-        http_component = RequestsHttp()
+        requests_session.get.return_value = Mock(Response, status_code=200, text=response_data)
+        http_component = RequestsHttp(lambda: requests_session)
         response = http_component.retrieve_text_resource(
             url=self._TEST_BASE_URL,
             url_params=url_params,
@@ -76,17 +77,15 @@ class TestRequestsHttp:
         assert response == response_data
 
         # Ensure that all parameters are forwarded and the timeout is set correctly
-        mocked_requests_get.assert_called_once_with(
+        requests_session.get.assert_called_once_with(
             url=self._TEST_BASE_URL,
             params=url_params,
             data=None,
-            headers=ANY,
             timeout=self._EXPECTED_REQUEST_TIMEOUT,
         )
 
         # Ensure that the expected headers are sent
-        headers = mocked_requests_get.call_args_list[0].kwargs["headers"]
-        assert headers.get("User-Agent") == self._EXPECTED_USER_AGENT
+        assert requests_session.headers.get("User-Agent") == self._EXPECTED_USER_AGENT
 
     @pytest.mark.parametrize(
         ("url_params", "query_content"),
@@ -131,10 +130,10 @@ class TestRequestsHttp:
     )
     def test_retrieve_json_resource_good(
         self,
-        mocked_requests_get: Mock,
         url_params: dict[str, str | int] | None,
         query_content: str | None,
         response_data: str,
+        requests_session: Mock,
     ) -> None:
         """
         Ensures the correct behaviour of the retrieve_json_resource() method for normal use cases:
@@ -143,8 +142,8 @@ class TestRequestsHttp:
          - The URL, their parameters and the query content must be forwarded correctly
          - The response content is returned as-is (no matter if it is valid JSON or not)
         """
-        mocked_requests_get.return_value = Mock(Response, status_code=200, text=response_data)
-        http_component = RequestsHttp()
+        requests_session.get.return_value = Mock(Response, status_code=200, text=response_data)
+        http_component = RequestsHttp(lambda: requests_session)
         response = http_component.retrieve_json_resource(
             url=self._TEST_BASE_URL,
             url_params=url_params,
@@ -155,18 +154,16 @@ class TestRequestsHttp:
         assert response == response_data
 
         # Ensure that all parameters are forwarded and the timeout is set correctly
-        mocked_requests_get.assert_called_once_with(
+        requests_session.get.assert_called_once_with(
             url=self._TEST_BASE_URL,
             params=url_params,
             data=query_content,
-            headers=ANY,
             timeout=self._EXPECTED_REQUEST_TIMEOUT,
         )
 
         # Ensure that the expected headers are sent
-        headers = mocked_requests_get.call_args_list[0].kwargs["headers"]
-        assert headers.get("User-Agent") == self._EXPECTED_USER_AGENT
-        assert headers.get("Accept") == "application/json"
+        assert requests_session.headers.get("User-Agent") == self._EXPECTED_USER_AGENT
+        assert requests_session.headers.get("Accept") == "application/json"
 
     @pytest.mark.parametrize(
         "send_request",
@@ -195,19 +192,157 @@ class TestRequestsHttp:
     )
     def test_retrieve_resource_errors(
         self,
-        mocked_requests_get: Mock,
         response_code_or_exception: int | Exception,
         send_request: Callable[[RequestsHttp, str], str],
+        requests_session: Mock,
     ) -> None:
         """
         Ensures that the retrieve_*_resource() methods raise an HttpRequestError in case of request
         or HTTP errors.
         """
-        mocked_requests_get.side_effect = (
+        requests_session.get.side_effect = (
             [Mock(Response, status_code=response_code_or_exception, reason="Fake Failure")]
             if isinstance(response_code_or_exception, int)
             else response_code_or_exception
         )
-        http_component = RequestsHttp()
+        http_component = RequestsHttp(lambda: requests_session)
         with pytest.raises(HttpRequestError):
             send_request(http_component, self._TEST_BASE_URL)
+
+    @pytest.mark.parametrize(
+        ("request_resources", "expected_session_calls"),
+        [
+            pytest.param(
+                [
+                    ("https://www.fomori.de/api/docs", {}),
+                    ("https://www.fomori.de/api/docs", {}),
+                ],
+                {"www.fomori.de": 2},
+                id="Same URL",
+            ),
+            pytest.param(
+                [
+                    ("https://www.fomori.de/api/docs1", {}),
+                    ("https://www.fomori.de/api/docs2", {}),
+                ],
+                {"www.fomori.de": 2},
+                id="Same host, different endpoints",
+            ),
+            pytest.param(
+                [
+                    ("https://www.fomori.de/api", {"tpye": "html"}),
+                    ("https://www.fomori.de/api", {"type": "json"}),
+                ],
+                {"www.fomori.de": 2},
+                id="Same endpoint, different parameters",
+            ),
+            pytest.param(
+                [
+                    ("https://www.example.com", {}),
+                    ("https://www.fomori.de", {}),
+                ],
+                {"www.example.com": 1, "www.fomori.de": 1},
+                id="Different hosts, single request each",
+            ),
+            pytest.param(
+                [
+                    ("https://www.example.com/api", {}),
+                    ("https://www.fomori.de", {"cat": 42}),
+                    ("https://www.example.com", {}),
+                    ("https://www.fomori.de/cat/37", {"type": "text"}),
+                    ("https://www.fomori.de", {}),
+                ],
+                {
+                    "www.example.com": 2,
+                    "www.fomori.de": 3,
+                },
+                id="Different hosts, multiple requests each",
+            ),
+            pytest.param(
+                [],
+                {},
+                id="No request",
+            ),
+        ],
+    )
+    def test_reuse_session(
+        self,
+        request_resources: list[tuple[str, dict[str, str | int] | None]],
+        expected_session_calls: dict[str, int],
+    ) -> None:
+        """
+        Ensure that the session is reused for subsequent requests to the same host.
+
+        - Each host gets its own session
+        - For the same host, the session is reused
+            - Even for different URL parameters
+            - Even for different API endpoints
+        - Sessions are created on demand only
+        """
+        created_sessions: list[Mock] = []  # All created created sessions
+
+        def create_session() -> Session:
+            nonlocal created_sessions
+            session = _create_session_mock()
+            session.get.return_value = Mock(Response, status_code=200, text="")
+            created_sessions.append(session)
+            return session
+
+        http_component = RequestsHttp(create_session)
+
+        # Do the appropriate number of requests
+        for url, params in request_resources:
+            http_component.retrieve_json_resource(
+                url=url,
+                url_params=params,
+            )
+
+        # Make sure the number of created sessions equals the number of expected hosts
+        assert len(created_sessions) == len(expected_session_calls)
+
+        def extract_hostname(url: str) -> str:
+            """Return the hostname part of the given url."""
+            return urlsplit(url).netloc
+
+        found_hosts: list[str] = []  # Stores the host names for which we already found a session
+        for session in created_sessions:
+            # Collect all host that were requested by this session
+            hosts = list(
+                {extract_hostname(call.kwargs["url"]) for call in session.get.call_args_list}
+            )
+
+            # Make sure that the get() calls to this session all go to the same host
+            assert len(hosts) == 1
+            hostname = hosts[0]
+
+            # Make sure this host name has not been requested by another session
+            assert hostname not in found_hosts
+
+            # Make sure a request to this nost name is expected at all
+            assert hostname in expected_session_calls
+
+            # Make sure the actual number of get() call of this host's session is as expected
+            assert session.get.call_count == expected_session_calls[hostname]
+
+            # Remember the host for the next iteration
+            found_hosts.append(hostname)
+
+
+@pytest.fixture
+def requests_session() -> Iterator[Mock]:
+    """
+    Fixture providing a mocked requests.Session created with `_create_session_mock()`. This fixture
+    is provided for convenience to make test cases with only one session a bit more readable.
+    """
+    return _create_session_mock()
+
+
+def _create_session_mock() -> Mock:
+    """
+    Create a mocked requests.Session containing a regular (but empty) `headers` dict. Using this
+    mock prevents the unit tests from doing real network requests, and allows them to control the
+    request behaviour.
+    """
+    mocked_session = Mock(spec=Session)
+    mocked_session.headers = {}
+    return mocked_session
