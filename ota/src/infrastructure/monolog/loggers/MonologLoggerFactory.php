becb71036db5118ce835d9c9bc843c9b88a3f991
Aardjon <58855074+aardjon@users.noreply.github.com>
2026-06-22 19:33:11 +0200

    feat(ota): Make it possible to shutdown the logging system
    This is useful for unit tests.

--- ota/src/infrastructure/monolog/loggers/MonologLoggerFactory.php
+++ ota/src/infrastructure/monolog/loggers/MonologLoggerFactory.php
@@ -28,12 +28,30 @@ final class MonologLoggerFactory implements LoggerFactory
      * Initialize and setup the logging system to use the provided [logHandler].
      *
      * Must be called exactly once at startup.
+     *
+     * @psalm-api
      */
     public static function setupLogging(AbstractLogHandler $logHandler): void
     {
         TradLogger::setLoggerFactory(new MonologLoggerFactory($logHandler));
     }
 
+    /**
+     * Shutdown the logging system.
+     *
+     * After that, no log messages can be processed anymore. This is mainly there for allowing a
+     * clean shutdown between unit test cases.
+     *
+     * @psalm-api
+     */
+    public static function shutdownLogging(): void
+    {
+        TradLogger::discardLoggerFactory();
+    }
+
+    /**
+     * Constructor for creating a new logger factory which uses the given [$logHandler].
+     */
     public function __construct(AbstractLogHandler $logHandler)
     {
         $this->logHandler = $logHandler;
