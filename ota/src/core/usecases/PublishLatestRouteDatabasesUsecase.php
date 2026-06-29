520f2adabe36b937cac9e9a6663dc440c69ec038
Aardjon <58855074+aardjon@users.noreply.github.com>
2026-06-29 19:24:20 +0200

    Fixup usecase

--- ota/src/core/usecases/PublishLatestRouteDatabasesUsecase.php
+++ ota/src/core/usecases/PublishLatestRouteDatabasesUsecase.php
@@ -8,6 +8,7 @@ use trad\core\boundaries\DbBuildService;
 use trad\core\boundaries\DbDirectoryRepository;
 use trad\core\boundaries\DbMetadataRepository;
 use trad\core\boundaries\PresentationBoundary;
+use trad\core\entities\ArtifactMetadata;
 use trad\core\entities\RouteDbMetadata;
 use trad\core\logging\Logger;
 use \DateInterval;
@@ -50,13 +51,7 @@ final class PublishLatestRouteDatabasesUsecase
     {
         $this->logger->info('Running use case publishLatestRouteDatabases()');
 
-        // Get the metadata of already published route databases
-        /** @var array<RouteDbMetadata> $availableRouteDbs */
-        $availableRouteDbs = [];
-        foreach ($this->directoryRepository->getRouteDbFiles() as $routeDbFile) {
-            $availableRouteDbs[] = $this->metadataRepository->getRouteDbMetadata($routeDbFile);
-        }
-
+        $availableRouteDbs = $this->getPublishedRouteDbFiles();
         if (count($availableRouteDbs) == 0) {
             $this->logger->info(
                 'No new route databases are available on the build service, stopping'
@@ -64,41 +59,19 @@ final class PublishLatestRouteDatabasesUsecase
 
             return;
         }
+        $latestRdbMetadata = $this->findLatestRouteDb($availableRouteDbs);
+        $oldestRdbMetadata = $this->findOldestRouteDb($availableRouteDbs);
 
-        // Find the newest and the oldest published route db
-        $latestRdbMetadata = $availableRouteDbs[0];
-        $oldestRdbMetadata = $availableRouteDbs[0];
-        foreach ($availableRouteDbs as $routeDbMetadata) {
-            if ($latestRdbMetadata->creationDate < $routeDbMetadata->creationDate) {
-                $latestRdbMetadata = $routeDbMetadata;
-            }
-            if ($oldestRdbMetadata->creationDate > $routeDbMetadata->creationDate) {
-                $oldestRdbMetadata = $routeDbMetadata;
-            }
-        }
-        $this->logger->info("Got latest routedb: {$latestRdbMetadata->downloadUrl}");
-
-        // Add one day to the search sarting date avoid publishing the same (newst) file again,
-        // because there is probably a gap between the DB (from the DB itself) and the artifact
-        // (from the build service) creation time.
-        $startDate = $latestRdbMetadata->creationDate->add(new DateInterval('P1D'));
-        // Now, get the metadata of all newer route databases available on the build service
-        $newArtifacts = $this->buildService->getNewerArtifactsMetadata($startDate);
+        $newArtifacts = $this->getNewerUnpublishedArtifacts($latestRdbMetadata);
         $newArtifactsCount = count($newArtifacts);
         if ($newArtifactsCount == 0) {
             $this->logger->warning('No workflow created a route database recently, stopping.');
 
             return;
         }
-        $this->logger->info("Got {$newArtifactsCount} new artifacts, publishing the latest one");
 
-        // Find the latest available artifact
-        $latestUnpublishedArtifact = $newArtifacts[0];
-        foreach ($newArtifacts as $artifact) {
-            if ($latestUnpublishedArtifact->creationDate < $artifact->creationDate) {
-                $latestUnpublishedArtifact = $artifact;
-            }
-        }
+        $this->logger->info("Got {$newArtifactsCount} new artifacts, publishing the latest one");
+        $latestUnpublishedArtifact = $this->findLatestArtifact($newArtifacts);
 
         $this->logger->info("Downloading latest artifact {$latestUnpublishedArtifact->identifier}");
         $newRouteDbArtifact = $this->buildService->getArtifactContent(
@@ -113,6 +86,94 @@ final class PublishLatestRouteDatabasesUsecase
         );
 
         $this->logger->info('Done');
+    }
+
+    /**
+     * Return the metadata of already published route databases.
+     *
+     * @return array<RouteDbMetadata>
+     */
+    private function getPublishedRouteDbFiles(): array
+    {
+        $availableRouteDbs = [];
+        foreach ($this->directoryRepository->getRouteDbFiles() as $routeDbFile) {
+            $availableRouteDbs[] = $this->metadataRepository->getRouteDbMetadata($routeDbFile);
+        }
+
+        return $availableRouteDbs;
+    }
+
+    /**
+     * Find and return the latest routedb from the [$availableRouteDbs] ones.
+     *
+     * @param array<RouteDbMetadata> $availableRouteDbs
+     */
+    private function findLatestRouteDb(array $availableRouteDbs): RouteDbMetadata
+    {
+        assert(! empty($availableRouteDbs));
+        $latestRdbMetadata = $availableRouteDbs[0];
+        foreach ($availableRouteDbs as $routeDbMetadata) {
+            if ($latestRdbMetadata->creationDate < $routeDbMetadata->creationDate) {
+                $latestRdbMetadata = $routeDbMetadata;
+            }
+        }
+        $this->logger->debug("Got latest routedb: {$latestRdbMetadata->downloadUrl}");
+
+        return $latestRdbMetadata;
+    }
+
+    /**
+     * Find and return the oldest routedb from the [$availableRouteDbs] ones.
+     *
+     * @param array<RouteDbMetadata> $availableRouteDbs
+     */
+    private function findOldestRouteDb(array $availableRouteDbs): RouteDbMetadata
+    {
+        assert(! empty($availableRouteDbs));
+        $oldestRdbMetadata = $availableRouteDbs[0];
+        foreach ($availableRouteDbs as $routeDbMetadata) {
+            if ($oldestRdbMetadata->creationDate > $routeDbMetadata->creationDate) {
+                $oldestRdbMetadata = $routeDbMetadata;
+            }
+        }
+        $this->logger->debug("Got oldest routedb: {$oldestRdbMetadata->downloadUrl}");
+
+        return $oldestRdbMetadata;
+    }
+
+    /**
+     * Return all artifacts form the build service that are newer than the provided
+     * [$latestRdbMetadata] by at least one day.
+     *
+     * Adding one day to the search starting date to avoid publishing the same (newest) file again,
+     * because there is probably a gap between the DB (from the DB itself) and the artifact (from
+     * the build service) creation time.
+     *
+     * @return array<ArtifactMetadata>
+     */
+    private function getNewerUnpublishedArtifacts(RouteDbMetadata $latestRdbMetadata): array
+    {
+        $startDate = $latestRdbMetadata->creationDate->add(new DateInterval('P1D'));
+
+        return $this->buildService->getNewerArtifactsMetadata($startDate);
+    }
+
+    /**
+     * Find and return the latest available artifact from the given [$availableArtifacts] ones.
+     *
+     * @param array<ArtifactMetadata> $availableArtifacts
+     */
+    private function findLatestArtifact(array $availableArtifacts): ArtifactMetadata
+    {
+        assert(! empty($availableArtifacts));
+        $latestUnpublishedArtifact = $availableArtifacts[0];
+        foreach ($availableArtifacts as $artifact) {
+            if ($latestUnpublishedArtifact->creationDate < $artifact->creationDate) {
+                $latestUnpublishedArtifact = $artifact;
+            }
+        }
+
+        return $latestUnpublishedArtifact;
     }
 }
 
