0c5a0c1e886a7ae1b1e3c034055e7bd848e9e5c3
Aardjon <58855074+aardjon@users.noreply.github.com>
2026-08-29 12:24:52 +0200

    refactor: Separate the raw data management from the actual processing in the OSM filter

--- scraper/src/trad/application/filters/source/osm/filter.py
+++ scraper/src/trad/application/filters/source/osm/filter.py
@@ -3,6 +3,7 @@ Filter for importing OSM data. Data is retrieved from Nominatim and Overpass.
 """
 
 from collections.abc import Callable, Collection, Iterable, Iterator
+from functools import partial
 from logging import getLogger
 from typing import Final, override
 
@@ -54,8 +55,145 @@ class OsmSummitDataFilter(SourceFilter):
     expect it to retrieve no data at all.
     """
 
-    _OVERPASS_PEAK_NODE_TAGS: Final = {"natural": "peak"}
-    """ OSM node tags by which we recognize a single summit point. """
+    def __init__(self, network_boundary: HttpNetworkingBoundary) -> None:
+        """
+        Create a new OsmSummitDataFilter instance that retrieves data via the given
+        [network_boundary].
+        """
+        super().__init__()
+        self._osm_api_receiver = OsmApiReceiver(http_boundary=network_boundary)
+        self._route_data_factory = RouteDataFactory(summit_sector_rank=1, summit_position_rank=1)
+
+    @override
+    def get_name(self) -> str:
+        return "OpenStreetMap"
+
+    @override
+    def _execute_source_filter(self, output_pipe: Pipe) -> None:
+        _logger.debug("'%s' filter started", self.get_name())
+        # Add the external source attribution
+        self.__store_external_source_attribution(output_pipe)
+
+        data_cache = _OsmDataCache()
+        data_cache.retrieve_all_data(self._osm_api_receiver)
+
+        self.__process_peak_nodes(data_cache, output_pipe)
+        self.__process_peak_relations(data_cache, output_pipe)
+
+        _logger.debug("'%s' filter finished", self.get_name())
+
+    def __process_peak_nodes(self, data_cache: _OsmDataCache, output_pipe: Pipe) -> None:
+        summits_from_nodes = self.__create_summits_from_nodes(
+            data_cache, data_cache.get_peak_nodes()
+        )
+        self.__store_summits(output_pipe, summits_from_nodes)
+        _logger.debug("Processed summits from %d nodes", len(data_cache.get_peak_nodes()))
+
+    def __process_peak_relations(self, data_cache: _OsmDataCache, output_pipe: Pipe) -> None:
+        # Create Summit objects for all relations
+        summits_from_relations = self.__create_summits_from_relations(
+            data_cache, data_cache.get_peak_relations()
+        )
+        # Send all created summits to the pipe
+        self.__store_summits(output_pipe, summits_from_relations)
+
+        _logger.debug("Processed summits from %d relations", len(data_cache.get_peak_relations()))
+
+    def __is_forbidden_node(self, osm_node: OverpassNode) -> bool:
+        """
+        Returns True if the given peak nodes may never be climbed on. This is checked by means of
+        legal restrictions, i.e. the 'access' tag.
+        For nodes with partial (e.g. seasonal) restrictions this method returns False because they
+        can indeed be accessed legally (just not always), and thus the restriction must be evaluated
+        by the mobile app dynamically.
+        """
+        total_access_restrictions: Final = ["no", "private"]
+        return osm_node.tags.access in total_access_restrictions
+
+    def __create_summits_from_relations(
+        self,
+        data_cache: _OsmDataCache,
+        peak_relations: Collection[OverpassRelation],
+    ) -> Iterator[Summit]:
+        """
+        Creates (and yields) a Summit object for each relation in `osm_relations`, using the peaks
+        from `osm_nodes`. The peak node elements that correspond to the processed relations are
+        removed from `osm_nodes`.
+        """
+        # Create Summit objects for all relations
+        for relation in peak_relations:
+            # Find the peak node member of this relations, should be exactly one
+            found_peak_nodes = data_cache.get_relation_member_nodes(
+                relation.id, lambda tags: tags.natural == "peak"
+            )
+
+            if not found_peak_nodes:
+                # The relation doesn't reference a "peak" node. This means we cannot get a position
+                # for it, which is bad.
+                raise DataProcessingError(
+                    f"No peak node can be loaded for relation '{relation.tags.name}'. Does it "
+                    "contain one at all?",
+                )
+            if len(found_peak_nodes) > 1:
+                # Not sure what this means, maybe we have to choose the correct one in the future?
+                # For now, just log a warning to find some examples.
+                _logger.warning(
+                    "Summit relation '%s' has multiple peak nodes (%d), using only the first one.",
+                    relation.tags.name,
+                    len(found_peak_nodes),
+                )
+            peak_node = found_peak_nodes[0]
+
+            if self.__is_forbidden_node(peak_node):
+                continue
+
+            yield self._route_data_factory.create_summit(
+                official_name=relation.tags.name,
+                alternate_names=relation.tags.get_alternate_names(),
+                position=GeoPosition.from_decimal_degree(peak_node.lat, peak_node.lon),
+                sector=data_cache.get_sector_name(relation.id),
+            )
+
+    def __create_summits_from_nodes(
+        self,
+        data_cache: _OsmDataCache,
+        osm_nodes: Iterable[OverpassNode],
+    ) -> Iterator[Summit]:
+        """
+        Creates (and yields) a Summit object for each node in `osm_nodes`.
+        """
+        for summit_element in osm_nodes:
+            if self.__is_forbidden_node(summit_element):
+                continue
+
+            yield self._route_data_factory.create_summit(
+                official_name=summit_element.tags.name,
+                alternate_names=summit_element.tags.get_alternate_names(),
+                position=GeoPosition.from_decimal_degree(summit_element.lat, summit_element.lon),
+                sector=data_cache.get_sector_name(summit_element.id),
+            )
+
+    def __store_external_source_attribution(self, pipe: Pipe) -> None:
+        pipe.add_source(self._EXTERNAL_SOURCE_DESCRIPTION)
+
+    def __store_summits(self, pipe: Pipe, summits: Iterable[Summit]) -> None:
+        for summit in summits:
+            try:
+                pipe.add_summit(summit)
+            except MergeConflictError as e:
+                _logger.warning(e)
+
+
+class _OsmDataCache:
+    """
+    Container that retrieves all necessary data from OSM and provides them to the filter. It does
+    some mapping (e.g. "which nodes are assigned to which relations"), but no further processing.
+
+    Unlike a usual cache, this class does not retrieve missing data as needed but gathers all data
+    at once, and only on explicit request. The main goal is to limit OSM API requests to the
+    absolutely necessary minimum. That alos means, that `retrieve_all_data()` must be called first
+    (and only once), otherwise the other methods won't return any results.
+    """
 
     _peak_nodes: dict[int, OverpassNode]
     """
@@ -88,48 +226,19 @@ class OsmSummitDataFilter(SourceFilter):
     belongs to.
     """
 
-    def __init__(self, network_boundary: HttpNetworkingBoundary) -> None:
-        """
-        Create a new OsmSummitDataFilter instance that retrieves data via the given
-        [network_boundary].
-        """
-        super().__init__()
-        self.__init_osm_data_caches()
-        self._osm_api_receiver = OsmApiReceiver(http_boundary=network_boundary)
-        self._route_data_factory = RouteDataFactory(summit_sector_rank=1, summit_position_rank=1)
-
-    @override
-    def get_name(self) -> str:
-        return "OpenStreetMap"
-
-    @override
-    def _execute_source_filter(self, output_pipe: Pipe) -> None:
-        _logger.debug("'%s' filter started", self.get_name())
-        # Add the external source attribution
-        self.__store_external_source_attribution(output_pipe)
-
-        self.__retrieve_osm_data()
-
-        self.__process_peak_nodes(output_pipe)
-        self.__process_peak_relations(output_pipe)
-
-        self.__init_osm_data_caches()
-
-        _logger.debug("'%s' filter finished", self.get_name())
-
-    def __init_osm_data_caches(self) -> None:
+    def __init__(self) -> None:
         self._sector_map = {}
         self._sector_names = {}
         self._peak_nodes = {}
         self._peak_relations = []
         self._relations_member_nodes = {}
 
-    def __retrieve_osm_data(self) -> None:
+    def retrieve_all_data(self, api_receiver: OsmApiReceiver) -> None:
         # Get the OSM ID of the geographical area to query
-        area_id = self.__get_area_id()
+        area_id = self.__get_area_id(api_receiver)
 
         # Get all OSM nodes and relations for that area
-        osm_elements = self.__get_osm_summit_elements(area_id)
+        osm_elements = self.__get_osm_summit_elements(api_receiver, area_id)
         _logger.debug("Retrieved %d OSM elements", len(osm_elements))
         if not osm_elements:
             return
@@ -139,49 +248,75 @@ class OsmSummitDataFilter(SourceFilter):
 
         # Get all parent relations (=sectors) of the given node and relation IDs
         self._sector_names, self._sector_map = self.__retrieve_sector_relations(
+            api_receiver,
             self._peak_nodes.keys(),
             [rel.id for rel in self._peak_relations],
         )
 
         # Move peak that are actually part of a relation into the `_relations_member_nodes` list
         self._relations_member_nodes = self.__extract_available_relation_member_nodes(
-            self._peak_nodes, self._peak_relations
+            self._peak_nodes,
+            self._peak_relations,
         )
 
         # Retrieve all missing peak relation members (relation members)
-        self.__retrieve_referenced_nodes(self._relations_member_nodes, self._peak_relations)
+        self.__retrieve_referenced_nodes(
+            api_receiver, self._relations_member_nodes, self._peak_relations
+        )
         _logger.debug("Retrieved all referenced nodes")
 
-    def __process_peak_nodes(self, output_pipe: Pipe) -> None:
-        summits_from_nodes = self.__create_summits_from_nodes(self._peak_nodes.values())
-        self.__store_summits(output_pipe, summits_from_nodes)
-        _logger.debug("Processed summits from %d nodes", len(self._peak_nodes))
+    def get_peak_nodes(self) -> list[OverpassNode]:
+        """
+        Return raw data of all retrieved peak nodes.
+        """
+        return list(self._peak_nodes.values())
 
-    def __process_peak_relations(self, output_pipe: Pipe) -> None:
-        # Create Summit objects for all relations
-        summits_from_relations = self.__create_summits_from_relations(
-            self._peak_relations, self._relations_member_nodes
-        )
+    def get_peak_relations(self) -> list[OverpassRelation]:
+        """
+        Return raw data of all retrieved peak relations.
+        """
+        return self._peak_relations
 
-        # Send all created summits to the pipe
-        self.__store_summits(output_pipe, summits_from_relations)
-        _logger.debug("Processed summits from %d relations", len(self._peak_relations))
+    def get_relation_member_nodes(
+        self, relation_id: int, predicate: Callable[[OverpassTags], bool] | None
+    ) -> list[OverpassNode]:
+        """
+        Return a list of all member node raw data assigned to the given relation. The given relation
+        ID must be of one of the relation returned by `get_peak_relations()`. If it is not, a
+        KeyError is raised.
 
-    def __get_area_id(self) -> int:
+        The `predicate` parameter allows to optionally filter the returned nodes by tags, i.e. only
+        member nodes with all of the given tag values (and optionally additional ones) are returned.
+        It is a callable that returns True for nodes that shall be returned.
+        """
+        if predicate is None:
+            predicate = partial(lambda _t: True)
+
+        return [node for node in self._relations_member_nodes[relation_id] if predicate(node.tags)]
+
+    def get_sector_name(self, osm_id: int) -> str | None:
+        sector_id = self._sector_map.get(osm_id)
+        if sector_id is not None:
+            return self._sector_names.get(sector_id)
+        return None
+
+    def __get_area_id(self, api_receiver: OsmApiReceiver) -> int:
         area_name: Final = "Sächsische Schweiz"
-        return int(self._osm_api_receiver.retrieve_area_by_name(area_name).osm_id)
+        return int(api_receiver.retrieve_area_by_name(area_name).osm_id)
 
-    def __get_osm_summit_elements(self, area_id: int) -> list[OverpassElement]:
+    def __get_osm_summit_elements(
+        self, api_receiver: OsmApiReceiver, area_id: int
+    ) -> list[OverpassElement]:
         """
         Retrieve all elements that represent Summits from OSM.
         Each summit is either a single node or a relation.
         """
         common_tags: Final = {"sport": "climbing", "climbing:trad": "yes"}
         element_filter: Final = {
-            OsmObjectTypes.node: self._OVERPASS_PEAK_NODE_TAGS | common_tags,
+            OsmObjectTypes.node: {"natural": "peak"} | common_tags,
             OsmObjectTypes.relation: {"type": "site", "climbing": "crag"} | common_tags,
         }
-        return self._osm_api_receiver.retrieve_elements_from_area(area_id, element_filter)
+        return api_receiver.retrieve_elements_from_area(area_id, element_filter)
 
     def __separate_elements_by_type(
         self, osm_elements: Collection[OverpassElement]
@@ -202,14 +337,14 @@ class OsmSummitDataFilter(SourceFilter):
         return osm_nodes, osm_relations
 
     def __retrieve_sector_relations(
-        self, node_ids: Collection[int], relation_ids: Collection[int]
+        self, api_receiver: OsmApiReceiver, node_ids: Collection[int], relation_ids: Collection[int]
     ) -> tuple[dict[int, str], dict[int, int]]:
         """
         Retrieves the sectors the given OSM elements belong to. The first returned value is a dict
         of sector ID and sector name, the second value is a dict of element IDs (dict key) and the
         ID of the sector they belong to (dict value).
         """
-        parent_relation_elements = self._osm_api_receiver.retrieve_parent_relations(
+        parent_relation_elements = api_receiver.retrieve_parent_relations(
             node_ids, relation_ids, rel_filter={"climbing": "area", "type": "site"}
         )
         sector_names = {rel.id: rel.tags.name for rel in parent_relation_elements if rel.tags.name}
@@ -245,6 +380,7 @@ class OsmSummitDataFilter(SourceFilter):
 
     def __retrieve_referenced_nodes(
         self,
+        api_receiver: OsmApiReceiver,
         relation_member_nodes: dict[int, list[OverpassNode]],
         peak_relations: Collection[OverpassRelation],
     ) -> None:
@@ -270,7 +406,7 @@ class OsmSummitDataFilter(SourceFilter):
         retrieved_nodes = (
             {
                 node.id: node
-                for node in self._osm_api_receiver.retrieve_nodes_by_ids(
+                for node in api_receiver.retrieve_nodes_by_ids(
                     osm_ids=missing_node_ids,
                     node_filter={},
                 )
@@ -287,98 +423,3 @@ class OsmSummitDataFilter(SourceFilter):
                     if member.ref in retrieved_nodes
                 ]
             )
-
-    def __is_forbidden_node(self, osm_node: OverpassNode) -> bool:
-        """
-        Returns True if the given peak nodes may never be climbed on. This is checked by means of
-        legal restrictions, i.e. the 'access' tag.
-        For nodes with partial (e.g. seasonal) restrictions this method returns False because they
-        can indeed be accessed legally (just not always), and thus the restriction must be evaluated
-        by the mobile app dynamically.
-        """
-        total_access_restrictions: Final = ["no", "private"]
-        return osm_node.tags.access in total_access_restrictions
-
-    def __create_summits_from_relations(
-        self,
-        peak_relations: Collection[OverpassRelation],
-        relations_member_nodes: dict[int, list[OverpassNode]],
-    ) -> Iterator[Summit]:
-        """
-        Creates (and yields) a Summit object for each relation in `osm_relations`, using the peaks
-        from `osm_nodes`. The peak node elements that correspond to the processed relations are
-        removed from `osm_nodes`.
-        """
-        # Create Summit objects for all relations
-        for relation in peak_relations:
-            # Find the peak node member of this relations, should be exactly one
-            found_peak_nodes = self.__find_matching_nodes(
-                relations_member_nodes[relation.id], lambda tags: tags.natural == "peak"
-            )
-
-            if not found_peak_nodes:
-                # The relation doesn't reference a "peak" node. This means we cannot get a position
-                # for it, which is bad.
-                raise DataProcessingError(
-                    f"No peak node can be loaded for relation '{relation.tags.name}'. Does it "
-                    "contain one at all?",
-                )
-            if len(found_peak_nodes) > 1:
-                # Not sure what this means, maybe we have to choose the correct one in the future?
-                # For now, just log a warning to find some examples.
-                _logger.warning(
-                    "Summit relation '%s' has multiple peak nodes (%d), using only the first one.",
-                    relation.tags.name,
-                    len(found_peak_nodes),
-                )
-            peak_node = found_peak_nodes[0]
-
-            if self.__is_forbidden_node(peak_node):
-                continue
-
-            yield self._route_data_factory.create_summit(
-                official_name=relation.tags.name,
-                alternate_names=relation.tags.get_alternate_names(),
-                position=GeoPosition.from_decimal_degree(peak_node.lat, peak_node.lon),
-                sector=self.__get_sector_name(relation.id),
-            )
-
-    def __find_matching_nodes(
-        self, nodes: Collection[OverpassNode], matches_tags: Callable[[OverpassTags], bool]
-    ) -> list[OverpassNode]:
-        # Remove all found points from `nodes`because they are not needed anymore
-        return [node for node in nodes if matches_tags(node.tags)]
-
-    def __create_summits_from_nodes(
-        self,
-        osm_nodes: Iterable[OverpassNode],
-    ) -> Iterator[Summit]:
-        """
-        Creates (and yields) a Summit object for each node in `osm_nodes`.
-        """
-        for summit_element in osm_nodes:
-            if self.__is_forbidden_node(summit_element):
-                continue
-
-            yield self._route_data_factory.create_summit(
-                official_name=summit_element.tags.name,
-                alternate_names=summit_element.tags.get_alternate_names(),
-                position=GeoPosition.from_decimal_degree(summit_element.lat, summit_element.lon),
-                sector=self.__get_sector_name(summit_element.id),
-            )
-
-    def __get_sector_name(self, osm_id: int) -> str | None:
-        sector_id = self._sector_map.get(osm_id)
-        if sector_id is not None:
-            return self._sector_names.get(sector_id)
-        return None
-
-    def __store_external_source_attribution(self, pipe: Pipe) -> None:
-        pipe.add_source(self._EXTERNAL_SOURCE_DESCRIPTION)
-
-    def __store_summits(self, pipe: Pipe, summits: Iterable[Summit]) -> None:
-        for summit in summits:
-            try:
-                pipe.add_summit(summit)
-            except MergeConflictError as e:
-                _logger.warning(e)
